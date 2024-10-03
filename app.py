3d4a3299c923189db52f8e7294e2cbccdb518b25
martenzo7 <martenzo7@proton.me>
2024-10-03 23:41:51 +0300

    setup

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/hello_world")
+def hello_world():
+    return "<p>Hello, World!</p>"
+@app.route("/")
+def index():
+    return render_template('index.html')
